d1a5d6d31e87876d5e4bd9a4ba291fad696f59ed
Anton Shamordin <shamordinanton@gmail.com>
2026-09-13 03:21:04 +0300

    fix: max update url

--- services/backend/src/report/max-updates.service.ts
+++ services/backend/src/report/max-updates.service.ts
@@ -7,7 +7,7 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { ReportService } from './report.service';
 
-const MAX_UPDATES_URL = 'https://platform-api2.max.ru/updates';
+const MAX_UPDATES_URL = 'https://max.ru/updates';
 const POLL_TIMEOUT_MS = 30;
 const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
